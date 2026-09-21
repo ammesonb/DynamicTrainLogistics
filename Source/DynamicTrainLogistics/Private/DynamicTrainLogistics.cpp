20176d9ea308af0d432e64a4b00babb8fcc935c0
Brett Ammeson <ammesonb@gmail.com>
2026-09-21 17:03:33 -0400

    Cmake conversion (#4)
    * Cmake conversion
    
    * Format and line endings in CI yml file

--- Source/DynamicTrainLogistics/Private/DynamicTrainLogistics.cpp
+++ Source/DynamicTrainLogistics/Private/DynamicTrainLogistics.cpp
@@ -4,17 +4,16 @@
 
 #define LOCTEXT_NAMESPACE "FDynamicTrainLogisticsModule"
 
-void FDynamicTrainLogisticsModule::StartupModule()
-{
-	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+void FDynamicTrainLogisticsModule::StartupModule() {
+    // This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin
+    // file per-module
 }
 
-void FDynamicTrainLogisticsModule::ShutdownModule()
-{
-	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
-	// we call this function before unloading the module.
+void FDynamicTrainLogisticsModule::ShutdownModule() {
+    // This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+    // we call this function before unloading the module.
 }
 
 #undef LOCTEXT_NAMESPACE
-	
+
 IMPLEMENT_MODULE(FDynamicTrainLogisticsModule, DynamicTrainLogistics)
--- Source/DynamicTrainLogistics/Public/DynamicTrainLogistics.h
+++ Source/DynamicTrainLogistics/Public/DynamicTrainLogistics.h
@@ -5,11 +5,9 @@
 #include "CoreMinimal.h"
 #include "Modules/ModuleManager.h"
 
-class FDynamicTrainLogisticsModule : public IModuleInterface
-{
+class FDynamicTrainLogisticsModule : public IModuleInterface {
 public:
-
-	/** IModuleInterface implementation */
-	virtual void StartupModule() override;
-	virtual void ShutdownModule() override;
+    /** IModuleInterface implementation */
+    virtual void StartupModule() override;
+    virtual void ShutdownModule() override;
 };
