819b9ad8690d4670f762927fe257f537e8a953b5
Brett Ammeson <ammesonb@gmail.com>
2026-09-22 00:15:13 -0400

    Keep generated module (#5)

--- Source/DynamicTrainLogistics/Private/DynamicTrainLogisticsModule.cpp
+++
@@ -1,7 +0,0 @@
-#include "DynamicTrainLogisticsModule.h"
-
-void FDynamicTrainLogisticsModule::StartupModule() {}
-
-void FDynamicTrainLogisticsModule::ShutdownModule() {}
-
-IMPLEMENT_GAME_MODULE(FDynamicTrainLogisticsModule, DynamicTrainLogistics);
